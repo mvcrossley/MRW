ff7e8606745b146bcfeb6c2fb50933129059ad2c
mvcrossley <mcrossl2@uwo.ca>
2017-04-10 20:07:20 -0400

    ADding thumbnails, form for comments

--- details.php
+++ details.php
@@ -13,8 +13,17 @@
 <head>
 <meta charset="UTF-8">
 <title><?php{$row['movie_title']}?></title>
+<link href="css/foundation.css" rel="stylesheet" type="text/css">
+<link href="css/app.css" rel="stylesheet" type="text/css">
+<script src="https://use.fontawesome.com/bffe9fd51f.js"></script>
 </head>
 <body>
+<?php
+    include("includes/header.html");
+?>
+
+<a href="index.php">Go Back to Movie List...</a><br>
+
 <?php
 
 	if(!is_string($getOne)){
@@ -24,13 +33,38 @@
 				 <p>{$row['movie_year']}</p><br>
 				 <p>{$row['movie_genre']}</p><br>
 				 <p>{$row['movie_role']}</p><br>
-				 <p>{$row['movie_desc']}</p><br>
-				 <a href=\"index.php\">Back...</a><br><br>";
-		
+				 <p>{$row['movie_desc']}</p><br>";	
 	}else{
-		echo "<p>{$getOne}</p>";	
+		echo "<p>{$getOne}</p>";
 	}
-
 ?>
+
+<h2>Post a Review:</h2>
+<?php if(!empty($message)){echo $message;} ?>
+	<form action="admin_details.php?id={$row['movie_id']}" method="post" enctype="multipart/form-data"><!--By default, forms are set to handle text. Set to multipart form data to handle other types of files-->
+		<label>Nickname:</label><br>
+		<input type="text" name="comment_user" value="" size="32"><br><br>
+		<label>Comment:</label><br>
+		<input type="text" name="comment_text" value="" size="32" ><br><br>
+		<label>Select Rating:</label><br>
+		<select name="comment_rating" >
+			<option value="">Please Select One...</option>
+			<option value="">&#9733;&#9734;&#9734;&#9734;&#9734;</option>
+			<option value="">&#9733;&#9733;&#9734;&#9734;&#9734;</option>
+			<option value="">&#9733;&#9733;&#9733;&#9734;&#9734;</option>
+			<option value="">&#9733;&#9733;&#9733;&#9733;&#9734;</option>
+			<option value="">&#9733;&#9733;&#9733;&#9733;&#9733;</option>
+	</select><br><br><br><br>
+	<input type="submit" name="submit" value="Add" >
+</form>
+
+<?php
+    include("includes/footer.html");
+?>
+
+<script src="js/app.js"></script>
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.js"></script>
 </body>
 </html>
